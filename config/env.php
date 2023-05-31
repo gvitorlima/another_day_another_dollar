15d4dc8d6fa89522b15b71b65899e37554e571a6
Gabriel Vitor <git624886@gmail.com>
2023-05-31 11:42:56 -0300

    fix: variáveis .env

--- config/env.php
+++ config/env.php
@@ -1,10 +1,12 @@
 <?php
 
 $envPatch = __DIR__ . '/../.env';
-$env = file($envPatch, 1);
+$env = file($envPatch, FILE_SKIP_EMPTY_LINES);
 
 foreach ($env as $key => $data) {
-  if (str_starts_with($data, '#')) {
+  $data = trim($data);
+
+  if (str_starts_with($data, '#') || empty($data)) {
     continue;
   }
 
