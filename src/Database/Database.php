5fbe9bacf94ac9673951a84831b83418fb37b5fe
Gabriel Vitor <git624886@gmail.com>
2023-05-31 11:43:52 -0300

    feat: inicio PDO connection

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -3,21 +3,45 @@
 namespace AnotherDay\Database;
 
 use AnotherDay\Database\DatabaseConfig;
+use Exception;
+use PDO;
 
 class Database
 {
   private string $stringConnection;
+  private DatabaseConfig $databaseConfig;
 
-  public function __construct(DatabaseConfig $database)
+  public function __construct(DatabaseConfig $databaseConfig)
   {
-    $this->stringConnection($database);
+    $this->databaseConfig = $databaseConfig;
+    $this->stringConnection($databaseConfig);
+
+    $this->connection();
   }
 
-  private function stringConnection(DatabaseConfig $database): void
+  private function stringConnection(DatabaseConfig $config): void
   {
-    $nameHost = $database->dsn() . $database->dbname() . $database->host();
-    $patch    = $database->path() . $database->dbname() . '.fdb;' . $database->charset() . ';dialect=1';
+    $nameHost = $config->dsn() . ':dbname=' . $config->host();
+    $patch    = $config->path() . $config->dbname() . '.fdb;charset=' . $config->charset() . ';dialect=3';
 
     $this->stringConnection = $nameHost . ':' . $patch;
+  }
+
+  private function connection()
+  {
+    try {
+
+      $pdo = new PDO(
+        $this->stringConnection,
+        $this->databaseConfig->user(),
+        $this->databaseConfig->password()
+      );
+
+    } catch (Exception $err) {
+      echo '<pre>';
+      print_r($err->getMessage());
+      echo '</pre>';
+      exit;
+    }
   }
 }
